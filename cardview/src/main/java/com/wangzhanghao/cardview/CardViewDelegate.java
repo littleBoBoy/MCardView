1938983286c4c1a5c2aa3e3ed17d01c2e18d3f00
wangzhanghao <837101207@qq.com>
2019-10-16 14:30:44 +0800

    first commit

---
+++ cardview/src/main/java/com/wangzhanghao/cardview/CardViewDelegate.java
@@ -0,0 +1,25 @@
+package com.wangzhanghao.cardview;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+interface CardViewDelegate {
+    void setCardBackground(Drawable var1);
+
+    Drawable getCardBackground();
+
+    boolean getUseCompatPadding();
+
+    boolean getPreventCornerOverlap();
+
+    void setShadowPadding(int var1, int var2, int var3, int var4);
+
+    void setMinWidthHeightInternal(int var1, int var2);
+
+    View getCardView();
+}
